b8f95c317a0c120d510191215535c353407ef055
Baek, Sehee <tpgmlqor@gmail.com>
2024-02-07 23:44:25 +0900

    복습 코드

--- programmers/practice/Lv2/뒤에있는큰수찾기.js
+++ programmers/practice/Lv2/뒤에있는큰수찾기.js
@@ -20,3 +20,25 @@ function solution(numbers) {
   answer.push(-1);
   return answer;
 }
+
+//  복습 코드
+//  시간초과
+
+function solution(numbers) {
+  let answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    let target = numbers.slice(i + 1, numbers.length);
+    if (target.length === 0) {
+      answer.push(-1);
+      break;
+    }
+    for (let j = 0; j < target.length; j++) {
+      if (numbers[i] < target[j]) {
+        answer.push(target[j]);
+        break;
+      } else if (j === target.length - 1) answer.push(-1);
+    }
+  }
+  return answer;
+}
