1e4beaf1df8bc222d102e26e9b736802672714ff
Baek, Sehee <tpgmlqor@gmail.com>
2024-02-14 13:09:02 +0900

    [복습]Lv2-땅따먹기

--- programmers/practice/Lv2/땅따먹기.js
+++ programmers/practice/Lv2/땅따먹기.js
@@ -88,3 +88,49 @@ function solution(land) {
 
   return Math.max(...answer);
 }
+
+// 복습 코드
+//  ⭕
+//  효율성도 통과되었지만
+//  for문 하나로 해결되는 코드가 있음
+function solution(land) {
+  let answer = 0;
+  let col = land[0].length;
+  let row = land.length;
+  console.log(col, row);
+  for (let i = 1; i < row; i++) {
+    let r = land[i - 1];
+    for (let j = 0; j < col; j++) {
+      let pre = r;
+      pre = Math.max(...pre.filter((e, i) => i != j));
+      land[i][j] += pre;
+    }
+  }
+
+  answer = Math.max(...land[row - 1]);
+
+  return answer;
+}
+
+//  Algorithm
+//  [JS] 프로그래머스 lv2. 땅따먹기
+//  2023. 2. 25. 10:57 - tistory
+//  참고
+function solution(land) {
+  let answer = 0;
+  let col = land[0].length;
+  let row = land.length;
+  console.log(col, row);
+  //  열이 col 4열이라는게 이미 정해져있기 때문에
+  //  for문 하나로도 가능함
+  for (let i = 1; i < row; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][1], land[i - 1][0], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][0]);
+  }
+
+  answer = Math.max(...land[row - 1]);
+
+  return answer;
+}
