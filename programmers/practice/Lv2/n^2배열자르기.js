5d564edb0a6abd2dc44434b8cf27e0b9038f59b8
Baek, Sehee <tpgmlqor@gmail.com>
2024-02-18 12:17:33 +0900

    [복복복습]Lv2-n^2 배열 자르기

--- programmers/practice/Lv2/n^2배열자르기.js
+++ programmers/practice/Lv2/n^2배열자르기.js
@@ -60,3 +60,17 @@ function solution(n, left, right) {
 
   return answer.slice(left, right + 1);
 }
+
+// 복복복습 코드 ⭕
+function solution(n, left, right) {
+  let answer = [];
+
+  for (let i = left; i <= right; i++) {
+    let x = Math.floor(i / n);
+    let y = i % n;
+
+    answer.push(Math.max(x, y) + 1);
+  }
+
+  return answer;
+}
