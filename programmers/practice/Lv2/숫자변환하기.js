59ec88fd7b118fede148de2a693833d4ac71b8f2
Baek, Sehee <tpgmlqor@gmail.com>
2024-02-19 22:25:37 +0900

    [복습]Lv2-숫자 변환하기

--- programmers/practice/Lv2/숫자변환하기.js
+++ programmers/practice/Lv2/숫자변환하기.js
@@ -45,3 +45,28 @@ function solution(x, y, n) {
 
   return dp[y] === Infinity ? -1 : dp[y];
 }
+
+//  복습 코드
+//  ⭕
+function solution(x, y, n) {
+  let answer = 0;
+
+  let dp = new Array(y + 1).fill(Infinity);
+  dp[x] = 0;
+
+  for (let i = x; i <= y; i++) {
+    if (i + n <= y) dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+    if (i * 2 <= y) dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+    if (i * 3 <= y) dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+    // if(dp[y] > 0) break;
+    //  내가 위의 코드를 적어서 테스트 1케이스에서 통과하지 못하고 있었음
+    //  두번째 케이스에서는 한 번의 연산으로 y값에 도달하고
+    //  세번째 케이스는 어차피 도달할 수 없기에
+    //  성공했던 거임
+    //  dp[y] 는 기존에 Infinity이므로 !!
+  }
+  //    console.log(dp);
+  // console.log(dp[y]);
+
+  return dp[y] !== Infinity ? dp[y] : -1;
+}
