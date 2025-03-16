7e7ca751927adac1e25799888bf9251b8f7b2277
Baek, Sehee <tpgmlqor@gmail.com>
2025-03-16 22:10:01 +0900

    [수정][BOJ]Lv3-서로 다른 부분 문자열의 개수

--- javaScript/boj/Lv3-서로다른부분문자열의개수.js
+++ javaScript/boj/Lv3-서로다른부분문자열의개수.js
@@ -18,3 +18,39 @@ while (len <= lines.length) {
 }
 
 console.log(set.size());
+
+// 내 코드 2
+// 시간 초과
+// Set 생성 방법 수정
+const fs = require("fs");
+let lines = fs.readFileSync(0).toString().split("\n");
+
+let str = lines[0];
+let len = 1;
+let set = new Set();
+
+while (len <= str.length) {
+  for (let i = 0; i <= str.length - len; i++) {
+    set.add(str.slice(i, i + len));
+  }
+}
+
+console.log(set.size());
+
+// 내 코드 3
+// 강의 자료에서 입력값 가지고 오기 복사함
+const fs = require("fs");
+let lines = fs.readFileSync(0).toString().split("\n");
+
+let str = lines[0];
+let len = 1;
+let set = new Set();
+
+while (len <= str.length) {
+  for (let i = 0; i <= str.length - len; i++) {
+    set.add(str.slice(i, i + len));
+  }
+  len++; // 증가를 안 적었어서 시간 초과났었다
+}
+
+console.log(set.size); //size 활용법이 틀렸었다
