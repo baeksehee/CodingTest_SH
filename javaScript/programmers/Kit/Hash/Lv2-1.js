03f3ba18ff711b400588908ef76f4626819ef445
Baek, Sehee <tpgmlqor@gmail.com>
2025-03-22 22:48:56 +0900

    [학교 강의 풀이][PG] Lv2-전화번호 목록

--- javaScript/programmers/Kit/Hash/Lv2-1.js
+++ javaScript/programmers/Kit/Hash/Lv2-1.js
@@ -61,3 +61,34 @@ function solution(phone_book) {
 
   return true;
 }
+
+// 코딩테스트지도도 강의 자료
+// Set을 사용한 풀이
+function solution(phone_book) {
+  let list = Array.from({ length: 21 }, () => []);
+  for (let no of phone_book) list[no.length].push(no);
+  let set = new Set();
+  for (let size = 1; size <= 20; ++size)
+    for (let no of list[size]) {
+      for (let i = 1; i < size; ++i)
+        if (set.has(no.substring(0, i))) return false;
+      set.add(no);
+    }
+  return true;
+}
+console.log(solution(["119", "97674223", "1195524421"]));
+console.log(solution(["123", "456", "789"]));
+console.log(solution(["12", "123", "1235", "567", "88"]));
+
+// 코딩테스트지도 강의 자료
+// 정렬로 풀이
+function solution(p) {
+  p.sort();
+  for (let i = 1; i < p.length; ++i)
+    if (p[i].length > p[i - 1].length && p[i].startsWith(p[i - 1]))
+      return false;
+  return true;
+}
+console.log(solution(["119", "97674223", "1195524421"]));
+console.log(solution(["123", "456", "789"]));
+console.log(solution(["12", "123", "1235", "567", "88"]));
